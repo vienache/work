02d8c7d5d5ef82fd03b5db56208d992fda09fe54
Enache, Viorel <viorel.enache@harman.com>
2018-02-15 14:15:08 +0000

    added boost::regex example

---
+++ boost_examples/regex/boost_regex_match.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <boost/regex.hpp>
+
+int main()
+{
+  try
+  {
+    boost::basic_regex<char, boost::cpp_regex_traits<char>> r;
+    r = "_E[0-9]+\\.[0-9]+_";
+
+    std::string s("_E209.002_");
+
+    if (boost::regex_match(s, r))
+    {
+      std::cout << "matched!" << std::endl;
+    }
+    else
+    {
+      std::cout << "not matched!" << std::endl;
+    }
+  }
+  catch (const std::exception& ex)
+  {
+    std::cout << "exception: " << ex.what() << std::endl;
+  }
+}
