d87a17f095aaf8234c17c12d00b77b3323feb8f7
Enache, Viorel <viorel.enache@harman.com>
2018-02-16 08:33:19 +0000

    added boost::algorithm::hex/unhex

---
+++ boost_examples/algorithm/hex_unhex.cpp
@@ -0,0 +1,27 @@
+#include <boost/algorithm/hex.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::vector<uint8_t> data = { 0x64, 0x61, 0x74, 0x61 };
+  std::string hex;
+
+  boost::algorithm::hex(data.begin(), data.end(), std::back_inserter(hex));
+
+  std::cout << "Hex data: [ " << hex << " ]" << std::endl;
+
+  assert(hex == "64617461");
+
+  //----------------------------------------------------
+
+  std::string unhex;
+  boost::algorithm::unhex(hex.begin(), hex.end(), std::back_inserter(unhex));
+
+  std::cout << "Unhex data: [ " << unhex << " ]" << std::endl;
+
+  assert(unhex == "data");
+
+  return 0;
+}
