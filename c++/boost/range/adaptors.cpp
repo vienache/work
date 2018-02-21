233da32b4bb67dc24de595ce4bdfd86095623cf8
Viorel Enache <viorel_enac@yahoo.com>
2018-02-21 21:41:39 +0200

    working on boost::range

--- c++/boost/range/adaptors.cpp
+++ c++/boost/range/adaptors.cpp
@@ -1,5 +1,6 @@
 #include <boost/range/algorithm.hpp>
 #include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm_ext.hpp>
 #include <array>
 #include <map>
 #include <string>
@@ -7,19 +8,41 @@
 #include <iterator>
 #include <iostream>
 
-int main()
+int main( int argc, char *argv[] )
 {
-  std::array<int, 3> a {{0, 1, 2}};
+  std::array<int, 3> a {0, 1, 2};
   std::map<std::string, int *> m;
   m.insert( std::make_pair( "a", &a[0] ) );
   m.insert( std::make_pair( "b", &a[1] ) );
   m.insert( std::make_pair( "c", &a[2] ) );
 
-  boost::copy( boost::adaptors::keys( m ),
+  //print map keys
+  boost::copy( m | boost::adaptors::map_keys,
                std::ostream_iterator<std::string> {std::cout, ","} );
   std::cout << '\n';
 
-  boost::copy( boost::adaptors::indirect( boost::adaptors::values( m ) ),
+  //print map values
+  boost::copy( m | boost::adaptors::map_values | boost::adaptors::indirected,
                std::ostream_iterator<int> {std::cout, ","} );
+  std::cout << '\n';
+
+  //or even better...
+  std::vector<int> v;
+  boost::push_back( v, m | boost::adaptors::map_values |
+                    boost::adaptors::indirected );
+  boost::copy( v, std::ostream_iterator<int> {std::cout, ","} );
+  std::cout << '\n';
+
+  //
+  std::vector<int> res;
+  boost::copy( boost::adaptors::unique( boost::adaptors::reverse( a ) ),
+               std::back_inserter( res ) );
+  boost::copy( res, std::ostream_iterator<int> {std::cout, ","} );
+  std::cout << '\n';
+
+  // more efficient (no extra allocations) + flexible
+  std::vector<int> vec;
+  boost::push_back( vec, a | boost::adaptors::reversed );
+  boost::copy( vec, std::ostream_iterator<int> {std::cout, ","} );
   std::cout << '\n';
 }
--- c++/boost/range/algorithms.cpp
+++ c++/boost/range/algorithms.cpp
@@ -1,6 +1,8 @@
 #include <boost/range/algorithm.hpp>
 #include <boost/range/numeric.hpp>
+#include <boost/range/algorithm_ext.hpp>
 #include <array>
+#include <deque>
 #include <iostream>
 
 int main()
@@ -13,10 +15,31 @@ int main()
 
   {
     std::cout << "----2. boost::max_element, boost::accumulate ----" << '\n';
-    std::array<int, 6> a {{0, 1, 2, 3, 4, 5}};
+    std::array<int, 6> a {0, 1, 2, 3, 4, 5};
     boost::random_shuffle( a );
     boost::copy( a, std::ostream_iterator<int> {std::cout, ","} );
     std::cout << "\n" << *boost::max_element( a ) << '\n';
     std::cout << boost::accumulate( a, 0 ) << '\n';
   }
+
+  //Range algorithms without counterparts in the standard library
+  {
+    std::cout << "----1. is_sorted, remove_erase, push_back ----" << '\n';
+    std::array<int, 6> a{0, 1, 2, 3, 4, 5};
+    std::cout << std::boolalpha << "is_sorted: " << boost::is_sorted( a ) << '\n';
+
+    std::deque<int> d;
+    boost::push_back( d, a );
+    boost::copy( d, std::ostream_iterator<int> {std::cout, ","} );
+    std::cout << '\n';
+
+    boost::remove_erase( d, 2 );
+    boost::copy( d, std::ostream_iterator<int> {std::cout, ","} );
+    std::cout << '\n';
+
+    boost::copy_n( d, 3, std::ostream_iterator<int> {std::cout, ","} );
+    boost::copy( d, std::ostream_iterator<int> {std::cout, ","} );
+    std::cout << '\n';
+  }
+
 }
