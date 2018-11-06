6163c7a8627bddd10480fa674674655861909fd6
Enache, Viorel <viorel.enache@harman.com>
2018-11-06 07:31:23 +0000

    working on apache

--- c/apache/modules/mod_test.c
+++ c/apache/modules/mod_test.c
@@ -13,18 +13,20 @@ module AP_MODULE_DECLARE_DATA test_module;
 static int outputFilter( ap_filter_t *f, apr_bucket_brigade *bb )
 {
     ap_log_cerror( APLOG_MARK, APLOG_WARNING, 0, f->c,
-                               "outputFilter next:%i", (f->next != 0) );
+                   "outputFilter next:%i", ( f->next != 0 ) );
     if ( f->r )
     {
-        //char *uri = ( char * ) ap_get_module_config( f->r->conn_config, &test_module );
-
         if ( f->r->uri )
         {
             ap_log_cerror( APLOG_MARK, APLOG_WARNING, 0, f->c,
                            "outputFilter: uri:%s, c:0x%lx, r:0x%lx",
-                           f->r->uri, ( long int )f->c, ( long int )f->r );
+                           f->r->uri,
+                           ( long int )f->c,
+                           ( long int )f->r );
         }
     }
+
+    ap_remove_output_filter( f );
 
     // Pass processing to next filters
     return ap_pass_brigade( f->next, bb );
@@ -36,7 +38,7 @@ static int test_pre_conn( conn_rec *c, void *csd )
                    "test_pre_conn: cip:%s, c:0x%lx",
                    c->client_ip, ( long int )c );
 
-    //ap_add_output_filter( OUT_FILTERNAME, NULL, NULL, c );
+    ap_add_output_filter( OUT_FILTERNAME, NULL, NULL, c );
 
     return OK;
 }
@@ -58,20 +60,36 @@ static int test_post_read_request( request_rec *r )
     return OK;
 }
 
-static void insert_output_filter(request_rec * r)
+static void insert_output_filter( request_rec *r )
 {
-    ap_add_output_filter( OUT_FILTERNAME, NULL, r, r->connection);
+    conn_rec *c = r->connection;
+    ap_log_cerror( APLOG_MARK, APLOG_WARNING, 0, c,
+                   "insert_output_filter: r:0x%lx, c:0x%lx",
+                   ( long int )r,
+                   ( long int )c );
+    ap_add_output_filter( OUT_FILTERNAME, NULL, r, r->connection );
 }
+
+//static int include_fixup(request_rec *r)
+//{
+//    conn_rec *c = r->connection;
+//    ap_log_cerror( APLOG_MARK, APLOG_WARNING, 0, c,
+//                   "include_fixup: r:0x%lx, c:0x%lx",
+//                   ( long int )r,
+//                   ( long int )c );
+//    ap_add_output_filter( OUT_FILTERNAME, NULL, r, r->connection);
+//    return DECLINED;
+//}
 
 static void test_register_hooks( apr_pool_t *p )
 {
     // http handling, with body
-    ap_register_output_filter( OUT_FILTERNAME, outputFilter, NULL, AP_FTYPE_PROTOCOL ) ;
+    ap_register_output_filter( OUT_FILTERNAME, outputFilter, NULL, AP_FTYPE_CONNECTION ) ;
 
     ap_hook_pre_connection( test_pre_conn, NULL, NULL, APR_HOOK_FIRST );
 
-    ap_hook_insert_filter( insert_output_filter, NULL, NULL, APR_HOOK_LAST );
-
+    ap_hook_insert_filter( insert_output_filter, NULL, NULL, APR_HOOK_FIRST );
+//    ap_hook_fixups(include_fixup, NULL, NULL, APR_HOOK_LAST);
     ap_hook_post_read_request( test_post_read_request, NULL, NULL, APR_HOOK_FIRST );
 }
 
